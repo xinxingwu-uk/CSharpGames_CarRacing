dcf4097b4f3b6289023a670a2bba4d083ebf7328
Xinxing Wu <xinxingwu@gmail.com>
2023-03-24 13:16:43 -0400

    Add files via upload

---
+++ Car6/CarRace/Car Racing Game MOO ICT/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Car_Racing_Game_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+
+        int roadSpeed;
+        int playerSpeed = 12;
+        int trafficSpeed;
+        int carImage;
+        int score;
+
+        bool goleft, goright;
+
+        Random rand = new Random();
+        Random carPosition = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            explosion.Visible = false;
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+            if (e.KeyCode == Keys.Q )
+            {
+                gameTimer.Stop();
+                btnStart.Enabled = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score: " + score;
+            score++;
+
+            if (goleft == true && player.Left > 10)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goright == true && player.Left < 415)
+            {
+                player.Left += playerSpeed;
+            }
+
+            roadTrack1.Top += roadSpeed;
+            roadTrack2.Top += roadSpeed;
+
+            if (roadTrack2.Top > 519)
+            {
+                roadTrack2.Top = -519;
+            }
+            if (roadTrack1.Top > 519)
+            {
+                roadTrack1.Top = -519;
+            }
+
+
+            AI1.Top += trafficSpeed;
+            AI2.Top += trafficSpeed;
+
+
+            if (AI1.Top > 530)
+            {
+                changeAIcars(AI1);
+            }
+
+            if (AI2.Top > 530)
+            {
+                changeAIcars(AI2);
+            }
+
+            if (player.Bounds.IntersectsWith(AI1.Bounds) || player.Bounds.IntersectsWith(AI2.Bounds))
+            {
+                gameTimer.Stop();
+                btnStart.Enabled = true;
+
+                playSound();
+                explosion.Visible = true;
+                player.Controls.Add(explosion);
+                explosion.Location = new Point(-8, 5);
+                explosion.BackColor = Color.Transparent;
+            }
+
+
+
+            if (score > 40 && score < 500)
+            {
+                roadSpeed = 15;
+                trafficSpeed = 17;
+            }
+
+
+            if (score > 500 && score < 2000)
+            {
+                roadSpeed = 20;
+                trafficSpeed = 22;
+            }
+
+            if (score > 2000)
+            {
+                trafficSpeed = 27;
+                roadSpeed = 25;
+            }
+
+        }
+
+
+        private void changeAIcars(PictureBox tempCar)
+        {
+
+            carImage = rand.Next(1, 9);
+
+            switch (carImage)
+            {
+
+                case 1:
+                    tempCar.Image = Properties.Resources.ambulance;
+                    break;
+                case 2:
+                    tempCar.Image = Properties.Resources.carGreen;
+                    break;
+                case 3:
+                    tempCar.Image = Properties.Resources.carGrey;
+                    break;
+                case 4:
+                    tempCar.Image = Properties.Resources.carOrange;
+                    break;
+                case 5:
+                    tempCar.Image = Properties.Resources.carPink;
+                    break;
+                case 6:
+                    tempCar.Image = Properties.Resources.CarRed;
+                    break;
+                case 7:
+                    tempCar.Image = Properties.Resources.carYellow;
+                    break;
+                case 8:
+                    tempCar.Image = Properties.Resources.TruckBlue;
+                    break;
+                case 9:
+                    tempCar.Image = Properties.Resources.TruckWhite;
+                    break;
+            }
+
+
+            tempCar.Top = carPosition.Next(100, 400) * -1;
+
+
+            if ((string)tempCar.Tag == "carLeft")
+            {
+                tempCar.Left = carPosition.Next(5, 200);
+            }
+            if ((string)tempCar.Tag == "carRight")
+            {
+                tempCar.Left = carPosition.Next(245, 422);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            goleft = false;
+            goright = false;
+
+            roadSpeed = 12;
+            trafficSpeed = 15;
+            score = 0;
+
+            explosion.Visible = false;
+
+            gameTimer.Start();
+
+            btnStart.Enabled = false;
+        }
+
+  
+        private void playSound()
+        {
+            System.Media.SoundPlayer playCrash = new System.Media.SoundPlayer(Properties.Resources.hit);
+            playCrash.Play();
+
+        }
+
+    }
+}
