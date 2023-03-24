9482fc58e1d9f505e8bbbb7bb392d8ea74a501b7
Xinxing Wu <xinxingwu@gmail.com>
2023-03-24 13:09:39 -0400

    Add files via upload

---
+++ Car1/CarRace/Car Racing Game MOO ICT/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Car_Racing_Game_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.roadTrack1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.roadTrack2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(46, 537);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(99, 41);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 10;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(359, 537);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(99, 41);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // roadTrack1
+            // 
+            this.roadTrack1.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
+            this.roadTrack1.Location = new System.Drawing.Point(0, -519);
+            this.roadTrack1.Name = "roadTrack1";
+            this.roadTrack1.Size = new System.Drawing.Size(475, 519);
+            this.roadTrack1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roadTrack1.TabIndex = 0;
+            this.roadTrack1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Controls.Add(this.roadTrack2);
+            this.panel1.Controls.Add(this.roadTrack1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(475, 519);
+            this.panel1.TabIndex = 0;
+            // 
+            // player
+            // 
+            this.player.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carYellow;
+            this.player.Location = new System.Drawing.Point(215, 402);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(50, 99);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 5;
+            this.player.TabStop = false;
+            // 
+            // roadTrack2
+            // 
+            this.roadTrack2.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
+            this.roadTrack2.Location = new System.Drawing.Point(3, -18);
+            this.roadTrack2.Name = "roadTrack2";
+            this.roadTrack2.Size = new System.Drawing.Size(475, 519);
+            this.roadTrack2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roadTrack2.TabIndex = 4;
+            this.roadTrack2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 624);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Car Racing Game MOO ICT";
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox roadTrack1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox roadTrack2;
+    }
+}
---
+++ Car1/CarRace/Car Racing Game MOO ICT/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_Racing_Game_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+
+        int roadSpeed;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnStart.Enabled = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            roadSpeed = 12;
+            gameTimer.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            gameTimer.Stop();
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            roadTrack1.Top += roadSpeed;
+            roadTrack2.Top += roadSpeed;
+
+            if (roadTrack2.Top > 519)
+            {
+                roadTrack2.Top = -519;
+            }
+            if (roadTrack1.Top > 519)
+            {
+                roadTrack1.Top = -519;
+            }
+        }
+
+    }
+}
